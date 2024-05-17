d602f1f12ad31d396dc0dba270a647d16d42fb19
dhrsana1 <74406967+dhrsana1@users.noreply.github.com>
2024-05-17 11:05:06 +0530

    Add files via upload

---
+++ src/main/java/cryptchat/server/dh/BrokenCrypto.java
@@ -0,0 +1,26 @@
+package cryptchat.server.dh;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+// vulnerability:  DES algorithm
+
+public class BrokenCrypto {
+	public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance(Transformations.DES.name());
+		KeyGenerator keyGen = KeyGenerator.getInstance(Transformations.valueOf("DES").toString());
+		System.out.println(Transformations.values()[1]);
+		cipher.init(Cipher.ENCRYPT_MODE, keyGen.generateKey());
+		String message = "Message";
+		byte[] encryptedMessage = cipher.doFinal(message.getBytes());
+		System.out.println("Original String -> " + message);
+		System.out.println("Encrypted String -> " + encryptedMessage);
+	}
+
+}
---
+++ src/main/java/cryptchat/server/dh/Transformations.java
@@ -0,0 +1,6 @@
+package cryptchat.server.dh;
+
+public enum Transformations {
+
+	DES, RC5, Blowfish, RC2, RC4
+}
